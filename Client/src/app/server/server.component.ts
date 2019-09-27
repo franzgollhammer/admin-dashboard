518792d3d37a170f22d54a85f5e44c3e9ea7077f
Franz Gollhammer <franz.gollhammer@gmail.com>
2019-09-27 19:24:22 +0200

    Updated server component

--- Client/src/app/server/server.component.ts
+++ Client/src/app/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { Server } from "selenium-webdriver/safari";
+import { Server } from "../models/server";
 
 @Component({
   selector: "app-server",
@@ -9,7 +9,29 @@ import { Server } from "selenium-webdriver/safari";
 export class ServerComponent implements OnInit {
   constructor() {}
 
+  color: string;
+  buttonText: string;
+
   @Input() serverInput: Server;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = "#66bb6a";
+      this.buttonText = "Shut Down";
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = "#ff6b6b";
+      this.buttonText = "Start";
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean) {
+    console.log(this.serverInput.name, ":", onlineStatus);
+    this.setServerStatus(!onlineStatus);
+  }
 }
